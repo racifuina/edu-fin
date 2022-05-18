6c805f7f6f3f2bb53dcd53dd809b81a710e5a48b
Rigoberto Acifuina <racifuina@me.com>
2022-05-17 22:37:01 -0600

    chore: Add LogBox ignore logs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { LogBox } from 'react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import './utils/firebase';
+LogBox.ignoreLogs(['AsyncStorage has been extracted', 'ViewPropTypes will be removed']);
 
 export default function App() {
     const isLoadingComplete = useCachedResources();
